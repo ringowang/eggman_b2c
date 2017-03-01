5964f9d845b7422b71c55bc8aff5e324cbcf0766
wangboguo <wangboguo@wangboguodeMacBook-Pro.local>
2017-03-01 18:16:16 +0800

    class_10_01_finished

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,22 @@
 class Product < ApplicationRecord
+  validates :category_id, presence: { message: '分类不能为空' }
+  validates :title, presence: { message: '名称不能为空' }
+  validates :status, inclusion: { in: %w(on,off), message: '状态必须是on | off' }
+  validates :amount, presence: { message: '库存不能为空' }
+  validates :amount, numericality: { only_integer: true, message: '库存必须为整数' }, if: proc { |product| !product.amount.blank? }
+  validates :msrp, presence: { message: 'MSRP不能为空' }
+  validates :msrp, numericality: { message: 'MSRP必须为数字' }, if: proc { |product| !product.msrp.blank? }
+  validates :price, presence: { message: '价格不能为空' }
+  validates :price, numericality: { message: '价格必须为数字' }, if: proc { |product| !product.price.blank? }
+  validates :description, presence: { message: '描述不能为空' }
+
   belongs_to :category
 
   # uuid生成
   before_create :set_default_attrs
 
   private
+
   def set_default_attrs
     self.uuid = RandomCode.generate_product_uuid
   end
